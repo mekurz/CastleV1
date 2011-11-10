49d39de74201a83244c1ee92946e7f5f7c9dcada
AChimp <achimp@gmail.com>
2011-11-09 22:27:11 -0600

    Version 21 - 09-Nov-2011 - Map Generator   - More unit tests and refactoring   - Added DoorMaker and some preliminary rules for adding entrances to rooms

--- MapGen/mapgen.js
+++ MapGen/mapgen.js
@@ -3,10 +3,13 @@ var MAP_HEIGHT = 100;
 var MIN_ROOM_SIZE = 5;
 var MAX_ROOM_SIZE = 13;
 
-var BLOCKED_CHAR = "#";
+var BLOCKED_CHAR = "B";
 var NOTHING_CHAR = "#";
 var ROOM_CHAR    = ".";
 var PERIMETER_CHAR = "O";
+var ENTRANCE_CHAR = "E";
+
+var max_room_id = 0;
 
 function Cell()
 {
@@ -89,6 +92,7 @@ function Room()
   {
     this.draw_perimeter( map );
     this.fill_room( map );
+    this.block_corners( map );
   };
   
   this.draw_perimeter = function( map )
@@ -116,9 +120,116 @@ function Room()
       }
     }
   };
+  
+  this.block_corners = function( map )  // TODO UNIT TEST
+  {
+    map[this.top_left.y - 1][this.top_left.x - 1].blocked = true;
+    map[this.top_left.y - 1][this.top_left.x + this.width].blocked = true;
+    map[this.top_left.y + this.height][this.top_left.x - 1].blocked = true;
+    map[this.top_left.y + this.height][this.top_left.x + this.width].blocked = true;
+  };
 }
 
-var max_room_id = 0;
+function DoorMaker( map, room )
+{
+  this.map = map;
+  this.room = room;
+  this.sills = new Array();
+  
+  this.get_num_doors = function()
+  {
+    var width = Math.floor( this.room.width / 2 );
+    var height = Math.floor( this.room.height / 2 );
+    var min_doors = Math.floor( Math.sqrt( width * height ) );
+    return Math.max( 0, min_doors + Math.floor( Math.random() * min_doors ) - this.count_existing_doors_for_room() );
+  };
+  
+  this.count_existing_doors_for_room = function()
+  {
+    var existing_doors = 0;
+    
+    for( var col = this.room.top_left.x; col < this.room.top_left.x + this.room.width; col++ )
+    {
+      existing_doors += map[this.room.top_left.y - 1][col].is_entrance;
+      existing_doors += map[this.room.top_left.y + this.room.height][col].is_entrance;
+    }
+    
+    for( var row = this.room.top_left.y; row < this.room.top_left.y + this.room.height; row++ )
+    {
+      existing_doors += map[row][this.room.top_left.x - 1].is_entrance;
+      existing_doors += map[row][this.room.top_left.x + this.room.width].is_entrance;
+    }
+    
+    return existing_doors;
+  };
+  
+  this.get_possible_door_sills = function()
+  {
+    this.add_north_sills();
+    this.add_south_sills();
+    this.add_east_sills();
+    this.add_west_sills();
+  };
+  
+  this.add_north_sills = function()
+  {
+    this.add_valid_horizontal_sills( this.room.top_left.y - 1, -1 ); 
+  };
+  
+  this.add_south_sills = function()
+  {
+    this.add_valid_horizontal_sills( this.room.top_left.y + this.room.height, 1 ); 
+  };
+  
+  this.add_east_sills = function()
+  {
+    this.add_valid_vertical_sills( this.room.top_left.x + this.room.width, 1 ); 
+  };
+  
+  this.add_west_sills = function()
+  {
+    this.add_valid_vertical_sills( this.room.top_left.x - 1, -1 ); 
+  };
+  
+  this.add_valid_horizontal_sills = function( row, direction )
+  {
+    if( row <= 1 || row >= MAP_HEIGHT-1 ) return;
+    
+    for( var col = this.room.top_left.x; col < this.room.top_left.x + this.room.width; col++ )
+    {
+      if( !this.map[row][col].blocked && !this.map[row][col].is_entrance && !this.map[row + direction][col].blocked )
+      {
+        this.sills.push( this.map[row][col] );
+      }
+    }
+  };
+  
+  this.add_valid_vertical_sills = function( col, direction )
+  {
+    if( col <= 1 || col >= MAP_WIDTH-1 ) return;
+    
+    for( var row = this.room.top_left.y; row < this.room.top_left.y + this.room.height; row++ )
+    {
+      if( !this.map[row][col].blocked && !this.map[row][col].is_entrance && !this.map[row][col + direction].blocked )
+      {
+        this.sills.push( this.map[row][col] );
+      }
+    }
+  };
+  
+  this.create_doors = function()
+  {    
+    var num_doors = this.get_num_doors();
+    this.get_possible_door_sills();
+    
+    for( var i = 0; i < num_doors; i++ )
+    {
+      var sill_num = Math.floor( Math.random() * this.sills.length );
+      this.sills[sill_num].is_entrance = true;
+      //this.sills.remove( sill_num );
+    }
+  };
+};
 
 //----------------------------------------------------------------------------------------------
 // MAP GENERATOR
@@ -182,37 +293,15 @@ function MapGenerator()
           room.top_left.x = ( col * 2 ) + 1;
           room.top_left.y = ( row * 2 ) + 1;
           
-          if( room.fits_on_map() && !room.contains_any_blocked_cell( this.map ) )
+          if( room.fits_on_map() && !room.contains_any_blocked_cell( this.map ) )     // TODO check for doors on the corners... don't allow!
           {
             room.place_room( this.map );
             this.rooms_list.push( room );
+            
+            var doors = new DoorMaker( this.map, room );
+            doors.create_doors();
           }
         }
-      }
-    }
-  };
-  
-  this.place_single_room = function( room )
-  {
-    // Draw room perimeter
-    for( var col = room.top_left.x - 1; col < room.top_left.x + room.width + 1; col++ )
-    {
-      this.map[room.top_left.y - 1][col].is_perimeter = true;
-      this.map[room.top_left.y + room.height][col].is_perimeter = true;
-    }
-    
-    for( var row = room.top_left.y - 1; row < room.top_left.y + room.height + 1; row++ )
-    {
-      this.map[row][room.top_left.x - 1].is_perimeter = true;
-      this.map[row][room.top_left.x + room.width].is_perimeter = true;
-    }
-    
-    // Fill room
-    for( var row = 0; row < room.height; row++ )
-    {
-      for( var col = 0; col < room.width; col++ )
-      {
-        this.map[row + room.top_left.y][col + room.top_left.x].room_id = room.room_id;
       }
     }
   };
@@ -239,6 +328,10 @@ function MapGenerator()
     if( cell.blocked )
     {
       return BLOCKED_CHAR;
+    }
+    else if( cell.is_entrance )
+    {
+      return ENTRANCE_CHAR;
     }
     else if( cell.is_perimeter )
     {
--- MapGen/tests.js
+++ MapGen/tests.js
@@ -10,6 +10,11 @@ function run_unit_tests()
   Room_contains_point();
   Room_fits_on_map();
   Room_contains_any_blocked_cell();
+  Room_draw_perimeter();
+  Room_fill_room();
+  
+  DoorMaker_count_existing_doors_for_room();
+  DoorMaker_add_valid_horizontal_sills();
 }
 
 function MapGenerator_allocate_map()
@@ -73,6 +78,13 @@ function MapGenerator_get_cell_character()
   {  
     var cell = new Cell();
     equals( mapgen.get_cell_character( cell ), NOTHING_CHAR, "Check Cell character - Nothing" );
+  });
+  
+  test( "Entrance Cell", function()  
+  {  
+    var cell = new Cell();
+    cell.is_entrance = true;
+    equals( mapgen.get_cell_character( cell ), ENTRANCE_CHAR, "Check Cell character - Entrance" );
   });
 }
 
@@ -298,4 +310,246 @@ function Room_contains_any_blocked_cell()
     room.height = 5; 
     ok( room.contains_any_blocked_cell( mapgen.map ), "Check that room is blocked" );
   });
+}
+
+function Room_draw_perimeter()
+{
+  module( "Room - draw_perimeter" );
+  var mapgen = new MapGenerator();
+  mapgen.allocate_map();
+  
+  var room = new Room();
+  room.top_left = new Point( 8, 8 );
+  room.width = 5;
+  room.height = 5;
+  
+  room.draw_perimeter( mapgen.map );
+  
+  test( "Perimeter is drawn", function()
+  {
+    var all_cells = true;
+     
+    for( var row = 0; row < MAP_HEIGHT && all_cells; row++ )
+    {
+      for( var col = 0; col < MAP_WIDTH && all_cells; col++ )
+      {
+        if( ( ( row == 7 || row == 13 ) && col >= 7 && col <= 13 )
+          ||( ( col == 7 || col == 13 ) && row >= 7 && row <= 13 ) )
+        {
+          all_cells = mapgen.map[row][col].is_perimeter;
+        }
+        else
+        {
+          all_cells = !mapgen.map[row][col].is_perimeter; 
+        }
+      }
+    }
+    
+    ok( all_cells, "Check that perimeter is drawn correctly" );
+  });
+}
+
+function Room_fill_room()
+{
+  module( "Room - fill_room" );
+  var mapgen = new MapGenerator();
+  mapgen.allocate_map();
+  
+  var room = new Room();
+  room.top_left = new Point( 8, 8 );
+  room.width = 5;
+  room.height = 5;
+  
+  room.fill_room( mapgen.map );
+  
+  test( "Room is filled", function()
+  {
+    var all_cells = true;
+     
+    for( var row = 0; row < MAP_HEIGHT && all_cells; row++ )
+    {
+      for( var col = 0; col < MAP_WIDTH && all_cells; col++ )
+      {
+        if( row >= 8 && row <= 12 && col >= 8 && col <= 12 )
+        {
+          all_cells = mapgen.map[row][col].is_a_room();
+        }
+        else
+        {
+          all_cells = !mapgen.map[row][col].is_a_room(); 
+        }
+      }
+    }
+    
+    ok( all_cells, "Check that the room is filled correctly" );
+  });
+}
+
+function DoorMaker_count_existing_doors_for_room()
+{
+  module( "DoorMaker - count_existing_doors_for_room" );
+  var mapgen = new MapGenerator();
+  mapgen.allocate_map();
+  
+  test( "Room has no doors", function()
+  {
+    var room = new Room();
+    room.top_left = new Point( 8, 8 );
+    room.width = 5;
+    room.height = 5;
+    room.place_room( mapgen.map );
+    
+    var doors = new DoorMaker( mapgen.map, room );
+    
+    equals( doors.count_existing_doors_for_room(), 0, "Check that there are no doors attached to a room" );
+  });
+  
+  test( "Corner cells don't get counted", function()
+  {
+    var room = new Room();
+    room.top_left = new Point( 8, 8 );
+    room.width = 5;
+    room.height = 5;
+    room.place_room( mapgen.map );
+    mapgen.map[7][7].is_entrance = true;
+    mapgen.map[7][13].is_entrance = true;
+    mapgen.map[13][7].is_entrance = true;
+    mapgen.map[13][13].is_entrance = true;
+    
+    var doors = new DoorMaker( mapgen.map, room );
+    
+    equals( doors.count_existing_doors_for_room(), 0, "Check that corner cells don't get counted for doors" );
+  });
+  
+  test( "Has 3 doors", function()
+  {
+    var room = new Room();
+    room.top_left = new Point( 8, 8 );
+    room.width = 5;
+    room.height = 5;
+    room.place_room( mapgen.map );
+    mapgen.map[7][10].is_entrance = true;
+    mapgen.map[10][13].is_entrance = true;
+    mapgen.map[13][10].is_entrance = true;
+    
+    var doors = new DoorMaker( mapgen.map, room );
+    
+    equals( doors.count_existing_doors_for_room(), 3, "Check that Room has 3 doors" );
+  });
+}
+
+function DoorMaker_add_valid_horizontal_sills()
+{
+  module( "DoorMaker - add_valid_horizontal_sills" );
+  var mapgen = new MapGenerator();
+  mapgen.allocate_map();
+  
+  test( "All NORTH is valid", function()
+  {
+    var room = new Room();
+    var doors = new DoorMaker( mapgen.map, room );
+    room.width = 5;
+    
+    room.top_left = new Point( 8, 3 );
+    doors.add_north_sills();
+    equals( doors.sills.length, 5, "Check that Room has 5 NORTH sills at ROW 3" );
+    doors.sills = new Array();
+    
+    room.top_left = new Point( 8, 8 );
+    doors.add_north_sills();
+    equals( doors.sills.length, 5, "Check that Room has 5 NORTH sills at ROW 8" );
+    doors.sills = new Array();
+  });
+  
+  test( "No NORTH is valid", function()
+  {
+    var room = new Room();
+    var doors = new DoorMaker( mapgen.map, room );
+    room.width = 5;
+    
+    room.top_left = new Point( 8, 0 );
+    doors.add_north_sills();
+    equals( doors.sills.length, 0, "Check that Room has 0 NORTH sills at ROW 0" );
+    doors.sills = new Array();
+    
+    room.top_left = new Point( 8, 1 );
+    doors.add_north_sills();
+    equals( doors.sills.length, 0, "Check that Room has 0 NORTH sills at ROW 1" );
+    doors.sills = new Array();
+    
+    room.top_left = new Point( 8, 2 );
+    doors.add_north_sills();
+    equals( doors.sills.length, 0, "Check that Room has 0 NORTH sills at ROW 2" );
+    doors.sills = new Array();
+  });
+  
+  test( "NORTH has 2 sills", function()
+  {
+    var room = new Room();
+    var doors = new DoorMaker( mapgen.map, room );
+       
+    room.top_left = new Point( 8, 8 );
+    room.width = 5;
+    
+    mapgen.map[7][8].blocked = true;    // One perimeter cell is blocked
+    mapgen.map[7][10].is_entrance = true;   // One perimeter cell is already an entrance (i.e. from a different room)
+    mapgen.map[6][11].blocked = true;   // One possible entrance would open onto a blocked cell
+    
+    doors.add_north_sills();
+    equals( doors.sills.length, 2, "Check that Room has 2 NORTH sills" );
+  });
+  
+  test( "All SOUTH is valid", function()
+  {
+    var room = new Room();
+    var doors = new DoorMaker( mapgen.map, room );
+    room.width = 5;
+    room.height = 0;
+    
+    room.top_left = new Point( 8, MAP_HEIGHT-2 );
+    doors.add_south_sills();
+    equals( doors.sills.length, 5, "Check that Room has 5 SOUTH sills at ROW MAP_HEIGHT-2" );
+    doors.sills = new Array();
+    
+    room.top_left = new Point( 8, 8 );
+    doors.add_south_sills();
+    equals( doors.sills.length, 5, "Check that Room has 5 SOUTH sills at ROW 8" );
+    doors.sills = new Array();
+  });
+  
+  test( "No SOUTH is valid", function()
+  {
+    var room = new Room();
+    var doors = new DoorMaker( mapgen.map, room );
+    room.width = 5;
+    room.height = 0;
+    
+    room.top_left = new Point( 8, MAP_HEIGHT-1 );
+    doors.add_south_sills();
+    equals( doors.sills.length, 0, "Check that Room has 0 SOUTH sills at ROW MAP_HEIGHT-1" );
+    doors.sills = new Array();
+    
+    room.top_left = new Point( 8, MAP_HEIGHT );
+    doors.add_south_sills();
+    equals( doors.sills.length, 0, "Check that Room has 0 SOUTH sills at ROW MAP_HEIGHT" );
+    doors.sills = new Array();
+  });
+  
+  test( "SOUTH has 2 sills", function()
+  {
+    var room = new Room();
+    var doors = new DoorMaker( mapgen.map, room );
+       
+    room.top_left = new Point( 8, 8 );
+    room.width = 5;
+    room.height = 5;
+    
+    mapgen.map[13][8].blocked = true;    // One perimeter cell is blocked
+    mapgen.map[13][10].is_entrance = true;   // One perimeter cell is already an entrance (i.e. from a different room)
+    mapgen.map[14][11].blocked = true;   // One possible entrance would open onto a blocked cell
+    
+    doors.add_south_sills();
+    equals( doors.sills.length, 2, "Check that Room has 2 SOUTH sills" );
+  });
+
 }
